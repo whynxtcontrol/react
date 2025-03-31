4ca5e7cf8abcd3efd254f8db8ea63b4cfc82f32f
whynxtcontrol <romilya03@gmail.com>
2025-03-31 20:10:00 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,4 @@ import "./main.css";
 
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
 
-// TODO: Реализовать компонент ProductList
 reactRoot.render(<ProductList products={products} />);
